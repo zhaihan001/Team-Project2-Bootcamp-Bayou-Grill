75688c8df193cad041304d837a88937a314d7d3f
James Zagorski <james_zagorski@yahoo.com>
2022-09-30 15:51:19 -0700

    phone number field added

--- public/js/login.js
+++ public/js/login.js
@@ -27,12 +27,13 @@ const signupFormHandler = async (event) => {
 
   const name = document.querySelector("#name-signup").value.trim();
   const email = document.querySelector("#email-signup").value.trim();
+  const phone = document.querySelector("#phone-signup").value.trim();
   const password = document.querySelector("#password-signup").value.trim();
 
   if (name && email && password) {
     const response = await fetch("/api/user", {
       method: "POST",
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({ name, email, phone, password }),
       headers: { "Content-Type": "application/json" },
     });
 
