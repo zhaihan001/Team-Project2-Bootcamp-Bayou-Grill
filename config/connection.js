0fc9588b6ac5b760c16022fbe4933ef97e500145
Han Zhai <zhaihan001@gmail.com>
2022-10-03 19:52:52 -0700

    try heroku again

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
+// const Runner = require("run-my-sql-file");
 
 let sequelize;
 
@@ -11,11 +12,26 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
     }
   );
 }
+
+// Runner.connectionOptions({
+//   host: process.env.DB_NAME,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+// });
+
+// const filepath = "./db/schema.sql";
+// Runner.runFile(filepath, (err) => {
+//   if (err) {
+//     console.log("unable to find the schema file");
+//   } else {
+//     console.log("Schema sucessfully sourced!");
+//   }
+// });
 
 module.exports = sequelize;
